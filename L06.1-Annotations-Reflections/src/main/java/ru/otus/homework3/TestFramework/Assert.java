b4a1f1f2afeea195508b8b4b27f6702a7d06f45d
timur <t.voytovi4@gmail.com>
2019-07-08 18:37:45 +0300

    Made exception separately

--- L06.1-Annotations-Reflections/src/main/java/ru/otus/homework3/TestFramework/Assert.java
+++ L06.1-Annotations-Reflections/src/main/java/ru/otus/homework3/TestFramework/Assert.java
@@ -1,40 +1,32 @@
 package ru.otus.homework3.TestFramework;
 
-public class Assert extends RuntimeException {
-    private String aCause;
+import ru.otus.homework3.TestFramework.Exceptions.AssertException;
 
-    private Assert(String cause) {
-        aCause = cause;
-    }
-
-    @Override
-    public String getMessage() {
-        return aCause;
-    }
+public class Assert {
 
     public static void assertEquals(Object expected, Object actual) {
         if (!expected.equals(actual))
-            throw new Assert("Objects are not equal. ");
+            throw new AssertException("Objects are not equal. ");
     }
 
     public static void assertTrue(boolean condition) {
         if (!condition)
-            throw new Assert("Condition is not true. ");
+            throw new AssertException("Condition is not true. ");
     }
 
     public static void assertTrue(String message, String condition) {
         if (!message.equals(condition))
-            throw new Assert("Conditions are not equal. ");
+            throw new AssertException("Conditions are not equal. ");
     }
 
     public static void assertNull(Object object) {
         if (object == null)
-            throw new Assert("Object is null. ");
+            throw new AssertException("Object is null. ");
     }
 
     public static void assertNotNull(Object object) {
         if (object != null)
-            throw new Assert("Object is not null. ");
+            throw new AssertException("Object is not null. ");
     }
 
 }
