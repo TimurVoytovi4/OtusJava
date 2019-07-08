a89fb74fe855745e48f5b46eabe4534565226800
timur <t.voytovi4@gmail.com>
2019-07-08 18:40:17 +0300

    Refactored: added logger instead file.txt made redundant logic in method

--- L06.1-Annotations-Reflections/src/main/java/ru/otus/homework3/TestFramework/TestRunner.java
+++ L06.1-Annotations-Reflections/src/main/java/ru/otus/homework3/TestFramework/TestRunner.java
@@ -1,12 +1,12 @@
 package ru.otus.homework3.TestFramework;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.homework3.Annotations.After;
 import ru.otus.homework3.Annotations.Before;
 import ru.otus.homework3.Annotations.Test;
+import ru.otus.homework3.TestFramework.Exceptions.TestException;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Objects;
@@ -14,69 +14,71 @@ import java.util.Objects;
 public class TestRunner {
     private Class clazz;
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     public TestRunner(Class clazz) {
         this.clazz = clazz;
     }
 
     private int passed = 0;
     private int failed = 0;
+    private Method before = null;
+    private Method after = null;
+
     public void run() {
-        try (FileWriter logWriter = new FileWriter("L06.1-Annotations-Reflections/src/main/"
-                + clazz.getSimpleName() + "_log.txt", false)) {
-
-            logWriter.write("* " + clazz.getName() + " * " + ":\n\n");
-
+        try {
             Method[] methods = clazz.getMethods();
-
-            Method before = null;
-            Method after = null;
-            for (Method method : methods) {
-                if (method.isAnnotationPresent(Before.class)) {
-                    before = method;
-                }
-                if (method.isAnnotationPresent(After.class)) {
-                    after = method;
-                }
-            }
+            methodNominator(methods);
             for (Method method : methods) {
                 if (method.isAnnotationPresent(Test.class)) {
                     Test test = method.getAnnotation(Test.class);
                     try {
-                        Objects.requireNonNull(before).invoke(before.getDeclaringClass().getConstructor().newInstance());
+                        callMethod(Objects.requireNonNull(before));
                         method.invoke(method.getDeclaringClass().getConstructor().newInstance());
                         passed++;
-                        callAfter(Objects.requireNonNull(after), logWriter);
                     } catch (InvocationTargetException e) {
                         Throwable exc = e.getTargetException();
                         if (exc.getClass().equals(test.expected())) {
                             passed++;
-                            callAfter(Objects.requireNonNull(after), logWriter);
+                            callMethod(Objects.requireNonNull(after));
                             continue;
                         }
                         failed++;
-                        logWriter.write("- Test " + method.getName() + ": " + exc.getMessage());
+                        logger.error("- Test " + method.getName() + ": " + exc.getMessage());
                     } catch (IllegalAccessException | IllegalArgumentException | InstantiationException e) {
                         failed++;
-                        logWriter.write("- Test " + method.getName() + ": " + e.getMessage());
+                        logger.error("- Test " + method.getName() + ": " + e.getMessage());
+                    } finally {
+                        callMethod(Objects.requireNonNull(after));
                     }
                 }
             }
-            logWriter.write("\n\nTotal: " + (failed + passed) + "\n" +
+            logger.info("\n\nTotal: " + (failed + passed) + "\n" +
                     "- Passed: " + passed + "\n" +
                     "- Failed: " + failed + "\n");
 
-            logWriter.flush();
-        } catch (IOException | NoSuchMethodException e) {
-            System.out.println("Lost access to the file");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
         }
     }
 
-    private void callAfter(Method after, FileWriter log) throws IOException {
+    private void methodNominator(Method[] methods) {
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Before.class))
+                if (before != null) throw new TestException();
+                else before = method;
+            if (method.isAnnotationPresent(After.class))
+                if (after != null) throw new TestException();
+                else after = method;
+        }
+    }
+
+    private void callMethod(Method method) {
         try {
-            after.invoke(after.getDeclaringClass().getConstructor().newInstance());
+            method.invoke(method.getDeclaringClass().getConstructor().newInstance());
         } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException e) {
             failed++;
-            log.write("- Test " + after.getName() + ": failed" + "\n");
+            logger.error("- Test " + method.getName() + ": failed" + "\n");
         }
     }
 }
