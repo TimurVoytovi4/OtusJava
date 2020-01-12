b4f8c144ac9ff07a9d9768a32b670403aec413fc
tim <t.voytovi4@gmail.com>
2020-01-12 04:27:09 +0200

    optimized and refactored

--- HW6-ATM/src/main/java/ru/otus/homework6/AtmUtil.java
+++ HW6-ATM/src/main/java/ru/otus/homework6/AtmUtil.java
@@ -3,38 +3,38 @@ package ru.otus.homework6;
 import ru.otus.homework6.Exceptions.AtmException;
 import ru.otus.homework6.Exceptions.NotRecognized;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class AtmUtil {
-    private List<Integer> temporaryOutCash = new ArrayList<>();
+
+    List<Integer> temporaryOutCash = new ArrayList<>();
 
     List<Integer> recognize(int cash) {
-        if (cash >= 500)
-            return fillCash(temporaryOutCash, 500, cash);
-        else if (cash >= 200)
-            return fillCash(temporaryOutCash, 200, cash);
-        else if (cash >= 100)
-            return fillCash(temporaryOutCash, 100, cash);
-        else if (cash >= 50)
-            return fillCash(temporaryOutCash, 50, cash);
-        else throw new NotRecognized(cash);
+        for (var nominal : Nominal.values()) {
+            int value = nominal.getTitle();
+            if (cash >= value) {
+                    for (int j = cash / value; j > 0; j--) temporaryOutCash.add(value);
+                    if (cash % value != 0) recognize(cash % value);
+                    return temporaryOutCash;
+            }
+        }
+        throw new NotRecognized(cash);
     }
 
     List<Integer> divide(int cash) {
-        if (cash != 50) {
-            if (cash == 500) return Arrays.asList(200, 200, 100);
-            if (cash == 200) return Arrays.asList(100, 100);
-            if (cash == 100) return Arrays.asList(50, 50);
+        if (cash!=50) {
+            List<Integer> tmp = new ArrayList<>();
+            for (var nominal : Nominal.values()) {
+                int value = nominal.getTitle();
+                if (cash == value) {
+                    int nextValue = Arrays.asList(Nominal.values()).get(nominal.ordinal() + 1).getTitle();
+                    for (int j = cash / nextValue; j > 0; j--) tmp.add(nextValue);
+                    if (cash % value != 0) tmp.add(cash % value);
+                    return tmp;
+                }
+            }
         }
         throw new AtmException("Невозможно выдать данную сумму.");
     }
 
-    List<Integer> fillCash(List<Integer> tmp, int nominal, int cash) {
-        int k = cash % nominal;
-        for (int j = cash / nominal; j > 0; j--) tmp.add(nominal);
-        if (k != 0) recognize(k);
-        return tmp;
-    }
 }
