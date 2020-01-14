befe9c6547a75d05448a63a9016e88241b534e88
tim <t.voytovi4@gmail.com>
2020-01-14 04:14:04 +0200

    deleted redundant class and refactored

--- HW6-ATM/src/main/java/ru/otus/homework6/MyATM.java
+++ HW6-ATM/src/main/java/ru/otus/homework6/MyATM.java
@@ -1,35 +1,50 @@
 package ru.otus.homework6;
 
+import ru.otus.homework6.Exceptions.NotRecognized;
+
 import java.util.*;
 
 public class MyATM implements ATM {
-    AtmProcessor pr = new AtmProcessor();
+
+    private final AtmUtil atmUtil = new AtmUtil();
+    private Map<Nominal, List<Nominal>> storage;
+
+    List<Nominal> fiveHundredsCell = new ArrayList<>();
+    List<Nominal> twoHundredsCell = new ArrayList<>();
+    List<Nominal> oneHundredsCell = new ArrayList<>();
+    List<Nominal> fiftiesCell = new ArrayList<>();
 
     public MyATM() {
-        List<Nominal> fiveHundredsCell = new ArrayList<>();
-        List<Nominal> twoHundredsCell = new ArrayList<>();
-        List<Nominal> oneHundredsCell = new ArrayList<>();
-        List<Nominal> fiftiesCell = new ArrayList<>();
-        pr.init(Arrays.asList(fiveHundredsCell,
+        this.storage = new EnumMap<>(Nominal.class);
+        Arrays.stream(Nominal.values()).forEach(var -> storage.put(var, Arrays.asList(fiveHundredsCell,
                 twoHundredsCell,
                 oneHundredsCell,
-                fiftiesCell));
+                fiftiesCell).get(var.ordinal())));
     }
 
     @Override
     public void set(int cash) {
-        pr.storeCash(cash);
+        Nominal nominal = atmUtil.findNominal(cash);
+        if (nominal != null) {
+            storage.get(nominal).add(atmUtil.findNominal(cash));
+        } else throw new NotRecognized(cash);
     }
 
     @Override
     public List<Integer> get(int cash) {
-        List<Integer> returnCash = new ArrayList<>(pr.giveCash(cash));
-        pr.outCash.clear();
-        return returnCash;
+        Nominal nominal = atmUtil.findNominal(cash);
+        List<Integer> outCash = new ArrayList<>();
+        if (nominal == null || storage.get(nominal).isEmpty()) {
+            atmUtil.divide(cash).forEach(this::get);
+        } else {
+            outCash.add(cash);
+            storage.get(nominal).remove(nominal);
+        }
+        return outCash;
     }
 
     @Override
     public int size() {
-        return pr.size();
+        return storage.values().stream().flatMap(Collection::stream).mapToInt(Nominal::getTitle).sum();
     }
 }
