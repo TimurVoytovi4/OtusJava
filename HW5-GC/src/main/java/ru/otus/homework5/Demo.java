1c0c0cde23e44ec0bab245f5bf9614114b12c17c
tim <t.voytovi4@gmail.com>
2020-01-16 06:14:22 +0200

    change filler method

--- HW5-GC/src/main/java/ru/otus/homework5/Demo.java
+++ HW5-GC/src/main/java/ru/otus/homework5/Demo.java
@@ -1,7 +1,7 @@
 package ru.otus.homework5;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ObjectProducer leakMem = new ObjectProducer(100000000);
         leakMem.run();
     }
--- HW5-GC/src/main/java/ru/otus/homework5/ObjectProducer.java
+++ HW5-GC/src/main/java/ru/otus/homework5/ObjectProducer.java
@@ -1,33 +1,23 @@
 package ru.otus.homework5;
 
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ObjectProducer {
     private int counter;
-    long collectionTime = 0;
 
     public ObjectProducer(int counter) {
         this.counter = counter;
     }
 
-    public void run() {
+    public void run() throws InterruptedException {
         List<Object> objects = new LinkedList<>();
         for (int i = 0; i < counter; i++) {
-            gcReport();
-            objects.add(new byte[10 * 1024 * 1024]);
-            if (collectionTime <= 120000000)
-                if (objects.size() > 30)
-                    objects.subList(0, objects.size() / 2).clear();
-        }
-    }
-
-    public void gcReport() {
-        for (GarbageCollectorMXBean bean :
-                ManagementFactory.getGarbageCollectorMXBeans()) {
-            collectionTime += bean.getCollectionTime();
+            Thread.sleep(100);
+            objects.add(new byte[1024 * 1024]);
+            if (i != 0 && i % 100 == 0) {
+                objects.subList(i / 3, objects.size() - 1).clear();
+            }
         }
     }
 }
